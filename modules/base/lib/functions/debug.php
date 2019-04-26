75eed40d1c9285b0424f44c886051f9720a8ad96
Tim <tim@itxplain.nl>
2019-04-26 09:33:28 +0200

    removed telegram api key

--- modules/base/lib/functions/debug.php
+++ modules/base/lib/functions/debug.php
@@ -9,10 +9,8 @@ function debug_admin_notification($str) {
     
     if (is_standalone_installation())
         return;
-    
-    $url = "https://api.telegram.org/bot571922944:AAGdtKEJHEtLK7HJcWQVg5vAY-FYSNwr4K8/sendMessage?chat_id=544128223&text=" . urlencode($str);
-    
-    get_url($url);
+ 
+    // TODO: some event...
 }
 
 
