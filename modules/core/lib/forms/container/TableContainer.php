dfffbda0cf6685b7b75f2c19ab02ffc95e73758d
Tim van Weezenbeek <tim@itxplain.nl>
2019-12-13 09:51:27 +0100

    form TableContainer

---
+++ modules/core/lib/forms/container/TableContainer.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace core\forms\container;
+
+
+use core\forms\WidgetContainer;
+
+class TableContainer extends WidgetContainer {
+    
+    protected $headerNames = array();
+    protected $rows = array();
+    
+    public function __construct($name) {
+        parent::__construct( $name );
+        
+    }
+    
+    
+    public function setHeaderNames($names) {
+        $this->headerNames = $names;
+    }
+    
+    
+    public function addRow($label, ... $args) {
+        $this->rows[] = array(
+            'label' => $label,
+            'widgets' => $args
+        );
+    }
+    
+    
+    public function render() {
+        $html = '';
+        
+        $html .= '<table class="clear table-container">';
+        $html .= '<thead>';
+        $html .= '<tr>';
+        foreach($this->headerNames as $n) {
+            // TODO: escape value?
+            $html .= '<td>'.$n.'</td>';
+        }
+        $html .= '</tr>';
+        $html .= '</thead>';
+        
+        for($x=0; $x < count($this->rows); $x++) {
+            $r = $this->rows[$x];
+            
+            $html .= '<tr>';
+            $html .= '<td>'.esc_html($r['label']).'</td>';
+            
+            foreach($r['widgets'] as $w) {
+                $html .= '<td>'.$w->render().'</td>';
+            }
+            $html .= '</tr>';
+        }
+        
+        $html .= '</table>';
+        
+        
+        return $html;
+    }
+    
+    
+}
