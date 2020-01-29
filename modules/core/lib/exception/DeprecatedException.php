23bf7a16d09df9803ce1bd812c5b602721ec805d
Tim van Weezenbeek <tim@itxplain.nl>
2020-01-29 15:18:49 +0100

    core, extra exception for deprecated functionality

---
+++ modules/core/lib/exception/DeprecatedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace core\exception;
+
+class DeprecatedException extends \Exception {
+    
+    public function __construct($message = null, $code = 0, \Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+    
+    
+}
+
