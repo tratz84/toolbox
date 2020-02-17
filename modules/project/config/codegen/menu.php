76a6331e47ce58b73c87f28115babb6ab299f73b
Tim <tim@itxplain.nl>
2020-02-17 08:41:57 +0100

    projects, inc/dec buttons start/end-time

--- modules/project/config/codegen/menu.php
+++ modules/project/config/codegen/menu.php
@@ -1,7 +1,7 @@
 <?php
 
 return array (
-  'treedata' => '[{"type":"default","text":"Projecten","data":{"object-locked":"0","form-name":"menu-generator-item-form","icon":"fa-tasks","label":"Projecten","url":"/?m=project&c=project","weight":"50","undefined":"Delete","as_first_child":""},"children":[{"type":"default","text":"Alle projecten","data":{"object-locked":"0","form-name":"menu-generator-item-form","icon":"fa-tags","label":"Alle projecten","url":"/?m=project&c=project","weight":"","undefined":"Delete"}},{"type":"default","text":"Maandoverzicht","data":{"object-locked":"0","form-name":"menu-generator-item-form","icon":"fa-tags","label":"Maandoverzicht","url":"/?m=project&c=monthly","weight":"","undefined":"Delete"}}]}]',
+  'treedata' => '[{"type":"default","text":"Projecten","data":{"object-locked":"0","form-name":"menu-generator-item-form","icon":"fa-tasks","label":"Projecten","url":"/?m=project&c=project","weight":"50","undefined":"Delete","as_first_child":""},"children":[{"type":"default","text":"Nieuw uur","data":{"object-locked":"0","form-name":"menu-generator-item-form","icon":"fa-tasks","label":"Nieuw uur","url":"/?m=project&c=projectHour&a=edit","weight":"","as_first_child":"","undefined":"Delete"}},{"type":"default","text":"Alle projecten","data":{"object-locked":"0","form-name":"menu-generator-item-form","icon":"fa-tags","label":"Alle projecten","url":"/?m=project&c=project","weight":"","undefined":"Delete"}},{"type":"default","text":"Maandoverzicht","data":{"object-locked":"0","form-name":"menu-generator-item-form","icon":"fa-tags","label":"Maandoverzicht","url":"/?m=project&c=monthly","weight":"","undefined":"Delete"}}]}]',
   'module_name' => 'project',
   'menu' => 
   array (
@@ -25,6 +25,22 @@ return array (
         0 => 
         array (
           'type' => 'default',
+          'text' => 'Nieuw uur',
+          'data' => 
+          array (
+            'object-locked' => '0',
+            'form-name' => 'menu-generator-item-form',
+            'icon' => 'fa-tasks',
+            'label' => 'Nieuw uur',
+            'url' => '/?m=project&c=projectHour&a=edit',
+            'weight' => '',
+            'as_first_child' => '',
+            'undefined' => 'Delete',
+          ),
+        ),
+        1 => 
+        array (
+          'type' => 'default',
           'text' => 'Alle projecten',
           'data' => 
           array (
@@ -37,7 +53,7 @@ return array (
             'undefined' => 'Delete',
           ),
         ),
-        1 => 
+        2 => 
         array (
           'type' => 'default',
           'text' => 'Maandoverzicht',
--- modules/project/templates/projectHour/edit.php
+++ modules/project/templates/projectHour/edit.php
@@ -28,7 +28,115 @@ $(document).ready(function() {
 	});
 
 	toggleFields();
+
+
+	var st_incMinute = $('<a href="javascript:void(0);" class="fa fa-plus"></a>');
+	st_incMinute.click(function() { adjust_time('[name=start_time]', '+', 'minute'); });
+	
+	var st_incHour = $('<a href="javascript:void(0);" class="fa fa-plus"></a>');
+	st_incHour.click(function() { adjust_time('[name=start_time]', '+', 'hour'); });
+	
+	var st_decMinute = $('<a href="javascript:void(0);" class="fa fa-minus"></a>');
+	st_decMinute.click(function() { adjust_time('[name=start_time]', '-', 'minute'); });
+
+	var st_decHour = $('<a href="javascript:void(0);" class="fa fa-minus dec-hour"></a>');
+	st_decHour.click(function() { adjust_time('[name=start_time]', '-', 'hour'); });
+	
+	st_incHour.insertAfter($('[name=start_time]'));
+	st_incMinute.insertAfter($('[name=start_time]'));
+	st_decMinute.insertAfter($('[name=start_time]'));
+	st_decHour.insertAfter($('[name=start_time]'));
+
+
+
+
+	var et_incMinute = $('<a href="javascript:void(0);" class="fa fa-plus"></a>');
+	et_incMinute.click(function() { adjust_time('[name=end_time]', '+', 'minute'); });
+	
+	var et_incHour = $('<a href="javascript:void(0);" class="fa fa-plus"></a>');
+	et_incHour.click(function() { adjust_time('[name=end_time]', '+', 'hour'); });
+	
+	var et_decMinute = $('<a href="javascript:void(0);" class="fa fa-minus"></a>');
+	et_decMinute.click(function() { adjust_time('[name=end_time]', '-', 'minute'); });
+
+	var et_decHour = $('<a href="javascript:void(0);" class="fa fa-minus dec-hour"></a>');
+	et_decHour.click(function() { adjust_time('[name=end_time]', '-', 'hour'); });
+	
+	et_incHour.insertAfter($('[name=end_time]'));
+	et_incMinute.insertAfter($('[name=end_time]'));
+	et_decMinute.insertAfter($('[name=end_time]'));
+	et_decHour.insertAfter($('[name=end_time]'));
+	
 });
+
+function adjust_time(field, method, type) {
+	var val = $(field).val();
+
+	if ($(field).attr('name') == 'end_time' && val == '') {
+		val = $('[name=start_time]').val();
+	}
+	
+
+	var date = <?= json_encode(date('d-m-Y')) ?>;
+	var time = '07:00';
+
+	if (val == '') {
+		$(field).val( date + ' ' + time );
+		return;
+	}
+	
+	if (val != '') {
+		var tokens = val.split(' ');
+
+		if (tokens.length == 2) {
+    		date = tokens[0];
+    		time = tokens[1];
+		}
+	}
+
+	var time_tokens = time.split(':');
+	var hour   = parseInt( time_tokens[0] );
+	var minuts = parseInt( time_tokens[1] );
+
+	if (type == 'hour') {
+		if (method == '+') {
+			hour++;
+		} else {
+			hour--;
+		}
+	}
+	
+	if (type == 'minute') {
+		if (method == '+') {
+    		minuts += 15;
+    		if (minuts >= 60) {
+    			minuts = 0;
+    			hour++;
+    		}
+		} else {
+			minuts -= 15;
+			
+			if (minuts < 0) {
+				hour--;
+				minuts = 45;
+			}
+		}
+	}
+
+	if (hour > 23) {
+		hour = 23;
+		minuts = 45;
+	}
+	if (hour < 0) {
+		hour = 0;
+		minuts = 0;
+	}
+
+	time = (hour < 10 ? '0' + hour : hour) + ':' + (minuts < 10 ? '0'+minuts : minuts);
+	
+	$(field).val(date + ' ' + time);
+}
+
 
 function toggleFields() {
 	var rt = $('[name=registration_type]:checked').val();
