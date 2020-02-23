f3e1e9e46fa80af5159f07864be38a1ac635548a
Tim van Weezenbeek <tim@itxplain.nl>
2020-02-23 17:15:19 +0100

    debug code removed

--- modules/codegen/controller/datamodel/editorController.php
+++ modules/codegen/controller/datamodel/editorController.php
@@ -15,22 +15,6 @@ class editorController extends BaseController {
         if (module_exists($this->mod) == false)
             throw new InvalidStateException('Module not found');
         
-        $tm = new TableModel('project', 'project');
-        $tm->addColumn('project_id', 'int');
-        $tm->setPrimaryKey('project_id');
-        $tm->addColumn('person_id', 'int');
-        $tm->addColumn('company_id', 'int');
-        $tm->addColumn('company_id', 'bigint');
-        $tm->addColumn('project_name', 'varchar(255)');
-        $tm->addColumn('active', 'bool');
-        // $tm->addColumn('note', 'text');
-        $tm->addColumn('edited', 'datetime');
-        $tm->addColumn('created', 'datetime');
-        
-        $mtg = new MysqlTableGenerator( $tm );
-        print $mtg->createTableUpdate();
-        
-        exit;
         
         return $this->render();
     }
