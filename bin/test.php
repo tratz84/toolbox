0abb3a1615a181910505c820313cd625d567903a
Tim van Weezenbeek <tim@itxplain.nl>
2020-03-16 19:31:46 +0100

    webmail, solr import dev..

--- bin/test.php
+++ bin/test.php
@@ -4,18 +4,10 @@
 include dirname(__FILE__).'/../config/config.php';
 
 
-
-// $t = new \base\model\User(5);
-// $t->read();
-
-// $t->setUsername('timbo123');
-// $t->save();
-
-use base\model\UserDAO;
-
-$uDao = new UserDAO();
-$users = $uDao->queryCursor("select * from base__user");
+bootstrapCli('dev');
 
 
-var_export($users->numRows());
+
+
+
 
