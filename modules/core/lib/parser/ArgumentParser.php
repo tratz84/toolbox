8ada31b47400d391cb52042192c9f9741b24cea8
Tim van Weezenbeek <tim@itxplain.nl>
2020-03-26 18:44:25 +0100

    ArgumentParser, long arguments support

--- modules/core/lib/parser/ArgumentParser.php
+++ modules/core/lib/parser/ArgumentParser.php
@@ -33,21 +33,26 @@ class ArgumentParser {
             $t = $this->tokens[$x];
             
             $t = trim($t);
-        
-            if (strpos($t, '--') === 0) {
+            
+            if (strpos($t, '--') === 0 && strlen($t) > 2) {
                 if (strlen($t) > 2) {
-                    $key = trim(substr($t, 2));
+                    $key = trim( trim(substr($t, 3)) );
                     $this->options[$key] = true;
                 }
             }
-            else if (strpos($t, '-') === 0) {
+            else if (strpos($t, '-') === 0 && strlen($t) > 1) {
                 if (strlen($t) > 1) {
                     $c = $t{1};
                     $this->options[$c] = trim(substr($t, 2));
+                    $key = trim( substr($t, 1) );
+                    if ($key)
+                        $this->options[$key] = true;
                 }
             }
         }
     }
+    
+    public function getOptions() { return $this->options; }
     
     public function hasOption($optionName) {
         return array_key_exists($optionName, $this->options) ? true : false;
