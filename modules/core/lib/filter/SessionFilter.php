264ec80204c3ee1dded09a9e751a077d1502f38c
Tim van Weezenbeek <tim@itxplain.nl>
2020-03-29 11:38:52 +0200

    SessionFilter fix, delete PHPSESSID-cookie when session_start() failes

--- modules/core/lib/filter/SessionFilter.php
+++ modules/core/lib/filter/SessionFilter.php
@@ -45,7 +45,24 @@ class SessionFilter {
             // api-calls are stateless
             
         } else {
-            session_start();
+            $r = session_start();
+            
+            if ($r == false) {
+                // creating session failed? this can happen on existing sessions that can't
+                // access the session-files anymore on the server. Can happen when the Apache-config
+                // changes and assigns other user-permissions to the php-pool or change the 
+                // AssignUserID-value in the <VirtualHosting>-settings
+                print get_template(module_file('core', 'templates/exception/handledError.php'), 
+                    [
+                        'message' => t('Error creating session')
+                    ]);
+                
+                // session_destroy(); <= this doesn't work in this case, because there's no access to the session-file
+                // unset PHPSESSID-cookie
+                setcookie(session_name(), null, 0, $sessionPath);
+                
+                exit;
+            }
         }
         
         $filterChain->next();
---
+++ modules/core/templates/exception/handledError.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Toolbox - Handled error</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+
+		<script src="<?= BASE_HREF ?>js/jquery-3.3.1.min.js" type="text/javascript"></script>
+		<script src="<?= BASE_HREF ?>js/bootstrap.min.js" type="text/javascript"></script>
+		<script src="<?= BASE_HREF ?>js/script.js" type="text/javascript"></script>
+		
+		<?php if (DEBUG || file_exists(WWW_ROOT.'/css/less/style.css') == false) : ?>
+		<link href="<?= BASE_HREF ?>css/less/base.less?v=<?= filemtime(WWW_ROOT.'/css/less/base.less') ?>" rel="stylesheet/less" type="text/css" />
+		<script>less = { env: 'development'};</script>
+		<script src="<?= BASE_HREF ?>lib/less/dist/less.js" type="text/javascript"></script>
+		<?php else : ?>
+		<link href="<?= BASE_HREF ?>css/less/style.css?v=<?= filemtime(WWW_ROOT.'/css/less/style.css') ?>" rel="stylesheet" type="text/css" />
+		<?php endif; ?>
+		
+		
+		<link href="<?= BASE_HREF ?>lib/font-awesome-4/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body class="auth">
+
+    	<div class="main-content">
+    	
+    		<div style="max-width: 800px; margin: 50px auto;">
+    			<?= t('An error has occured') ?>: <?= $message ?>
+    			<br/>
+    			<br/>
+    			<a href="<?= isset($url) ? $url : appUrl('/') ?>"><?= t('Click here to try again') ?></a>
+    		</div>
+    		
+    	</div>
+
+	</body>
+</html>
+
