535726c3cf18d5bfd6ff8ad9c5afe9e989ddea69
Tim van Weezenbeek <tim@itxplain.nl>
2020-04-21 20:40:39 +0200

    invoice, dashboard widget styling

--- modules/invoice/templates/dashboardWidgets/lastInvoices.php
+++ modules/invoice/templates/dashboardWidgets/lastInvoices.php
@@ -11,11 +11,15 @@
 <table class="list-widget" style="width: 100%;">
 	<thead>
 		<tr>
-			<th><?= t('Number_short') ?></th>
-			<th><?= t('Name') ?></th>
-			<th><?= t('Description') ?></th>
+			<th class="th-invoice-nr-name">
+				<div><?= t('Number_short') ?></div>
+				<div><?= t('Name') ?></div>
+			</th>
+			<th class="th-invoice-nr"><?= t('Number_short') ?></th>
+			<th class="th-name"><?= t('Name') ?></th>
+			<th class="th-description"><?= t('Description') ?></th>
 			<?php if ($widgetSettings['show_invoice_amount']) : ?>
-			<th style="text-align: right; padding-right: 10px;">
+			<th class="th-amount" style="text-align: right; padding-right: 10px;">
 				Bedrag
 				
 				<?php if ($invoiceSettings->getPricesIncVat()) : ?>
@@ -25,26 +29,33 @@
 				<?php endif; ?>
 			</th>
 			<?php endif; ?>
-			<th><?= t('State') ?></th>
+			<th class="th-state"><?= t('State') ?></th>
 			<?php if ($widgetSettings['show_open_days']) : ?>
-			<th>Dagen</th>
+			<th class="th-days">Dagen</th>
 			<?php endif; ?>
-			<th><?= strOrder(1) ?><?= t('date') ?></th>
+			<th class="th-invoice-date"><?= strOrder(1) ?><?= t('date') ?></th>
 		</tr>
 	</thead>
 	<tbody>
     	<?php foreach($invoices->getObjects() as $i) : ?>
     	<tr onclick="window.location=appUrl('/?m=invoice&c=invoice&a=edit&id=<?= $i['invoice_id'] ?>')" class="clickable">
-    		<td style="padding-right: 10px;">
+			<td class="td-invoice-nr-name">
+				<div><?= esc_html( $i['invoiceNumberText'] ) ?></div>
+				<div>
+					<?= esc_html($i['company_name']) ?>
+	    			<?= esc_html(format_personname($i)) ?>
+				</div>
+			</td>
+    		<td class="td-invoice-nr" style="padding-right: 10px;">
     			<?= esc_html( $i['invoiceNumberText'] ) ?>
     		</td>
-    		<td>
+    		<td class="td-name">
     			<?= esc_html($i['company_name']) ?>
     			<?= esc_html(format_personname($i)) ?>
     		</td>
-    		<td><?= esc_html($i['subject']) ?></td>
+    		<td class="td-description"><?= esc_html($i['subject']) ?></td>
     		<?php if ($widgetSettings['show_invoice_amount']) : ?>
-			<td style="text-align: right; padding-right: 10px;">
+			<td class="td-amount" style="text-align: right; padding-right: 10px;">
 				<?php if ($invoiceSettings->getPricesIncVat()) : ?>
 					<?= format_price($i['total_calculated_price_incl_vat'], true, ['thousands' => '.']) ?>
 				<?php else : ?>
@@ -52,11 +63,11 @@
 				<?php endif; ?>
 			</td>
 			<?php endif; ?>
-    		<td><?= esc_html($i['invoice_status_description']) ?></td>
+    		<td class="td-state"><?= esc_html($i['invoice_status_description']) ?></td>
     		<?php if ($widgetSettings['show_open_days']) : ?>
-			<td><?= days_between($i['invoice_date'], date('Y-m-d')) ?></td>
+			<td class="td-days"><?= days_between($i['invoice_date'], date('Y-m-d')) ?></td>
 			<?php endif; ?>
-    		<td><?= format_date($i['invoice_date'], 'd-m-Y') ?></td>
+    		<td class="td-invoice-date"><?= format_date($i['invoice_date'], 'd-m-Y') ?></td>
     	</tr>
     	<?php endforeach; ?>
         <?php if (count($invoices->getObjects())==0) : ?>
