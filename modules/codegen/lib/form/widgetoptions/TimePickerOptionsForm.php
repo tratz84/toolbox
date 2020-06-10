90d7b6c4a77c6078855d426a5d5f478322dad71f
Tim van Weezenbeek <tim@itxplain.nl>
2020-06-10 09:00:57 +0200

    space-o :D

--- modules/codegen/lib/form/widgetoptions/TimePickerOptionsForm.php
+++ modules/codegen/lib/form/widgetoptions/TimePickerOptionsForm.php
@@ -13,7 +13,8 @@ class TimePickerOptionsForm extends DefaultWidgetOptionsForm {
     public function __construct() {
         parent::__construct();
         
-        $this->removeWidget('defaultValue');        $this->addWidget(new TimePickerField('defaultValue', '', t('Default time')));
+        $this->removeWidget('defaultValue');
+        $this->addWidget(new TimePickerField('defaultValue', '', t('Default time')));
     }
     
 }
