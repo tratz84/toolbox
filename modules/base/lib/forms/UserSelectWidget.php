af01fbefd57797c0765109f6c969c192e3596709
Tim van Weezenbeek <tim@itxplain.nl>
2020-06-12 16:19:18 +0200

    base, UserSelectWidget dev.. base, css info popup z-index

--- modules/base/lib/forms/UserSelectWidget.php
+++ modules/base/lib/forms/UserSelectWidget.php
@@ -3,6 +3,7 @@
 namespace base\forms;
 
 use core\forms\DynamicSelectField;
+use base\service\UserService;
 
 class UserSelectWidget extends DynamicSelectField {
     
@@ -14,6 +15,31 @@ class UserSelectWidget extends DynamicSelectField {
         if ($label == null) $label = t('User');
         
         parent::__construct($name, $defaultValue, $defaultText, $endpoint, $label);
+    }
+    
+    
+    public function bindObject($obj) {
+        parent::bindObject($obj);
+        
+        $userId = null;
+        
+        if (method_exists($obj, 'getUserId')) {
+            $userId = $obj->getUserId();
+        }
+        
+        if (is_array($obj) && isset($obj['user_id'])) {
+            $userId = $obj['user_id'];
+        }
+        
+        if ($userId) {
+            $userService = object_container_get(UserService::class);
+            $user = $userService->readUser( $userId );
+            
+            if ($user) {
+                $this->setDefaultText( (string)$user );
+            }
+        }
+        
     }
     
 }
