f30e18dbdf86bfddc29d7d729b999f9aa58f5a22
Tim van Weezenbeek <tim@itxplain.nl>
2020-06-17 11:39:59 +0200

    filesync, translations

--- modules/filesync/lang/nl_NL.php
+++ modules/filesync/lang/nl_NL.php
@@ -9,6 +9,11 @@ $l['New file']      = 'Nieuw bestand';
 $l['Existing file'] = 'Bestaand bestand';
 $l['Unset']         = 'Reset';
 
+$l['Filename']      = 'Bestandsnaam';
+$l['File size']     = 'Bestandsgrootte';
+$l['Document date'] = 'Document datum';
+
+
 
 return $l;
 
