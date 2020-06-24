26721ad4d29c2a1503d17c9780e3a92ea7827c93
Tim van Weezenbeek <tim@itxplain.nl>
2020-06-24 10:51:09 +0200

    project, check permissions top icon mobile

--- modules/project/autoload.php
+++ modules/project/autoload.php
@@ -15,7 +15,12 @@ Context::getInstance()->enableModule('project');
 
 $oc = ObjectContainer::getInstance();
 
-hook_register_javascript('project', BASE_HREF.'module/project/js/script.js');
+// access to project?
+$jsStr = '';
+if (hasCapability('core', 'userType.user')) {
+    $jsStr = '?phicon=1';
+}
+hook_register_javascript('project', BASE_HREF.'module/project/js/script.js'.$jsStr);
 hook_htmlscriptloader_enableGroup('project');
 
 
--- modules/project/public/js/script.js
+++ modules/project/public/js/script.js
@@ -6,10 +6,14 @@
 
 $(document).ready(function() {
 	
-	var anchorNewPh = $('<a class="fa fa-clock-o"></a>');
-	anchorNewPh.attr('href', appUrl('/?m=project&c=projectHour&a=edit'));
-	
-	$('.mobile-icon-container').append(anchorNewPh);
+	// check if icon has to be set (permission thing)
+	var js = $(document).find('script[src*="project/js/script.js"]');
+	if (js.attr('src').toString().indexOf('phicon=1') != -1) {
+		var anchorNewPh = $('<a class="fa fa-clock-o"></a>');
+		anchorNewPh.attr('href', appUrl('/?m=project&c=projectHour&a=edit'));
+		
+		$('.mobile-icon-container').append(anchorNewPh);
+	}
 	
 	
 });
