9bfe7d1220f1860729a6ed9d4744ed7c09755c59
Tim van Weezenbeek <tim@itxplain.nl>
2020-07-09 16:38:01 +0200

    customer, lang

--- modules/customer/lang/nl_NL.php
+++ modules/customer/lang/nl_NL.php
@@ -8,6 +8,7 @@ $lang['Customer']        = 'Klant';
 $lang['New customer']    = 'Klant aanmaken';
 $lang['User/Customer']   = 'Gebruiker/klant';
 
+$lang['Customer overview'] = 'Klanten overzicht';
 $lang['Company settings'] = 'Bedrijfsinstellingen';
 
 return $lang;
