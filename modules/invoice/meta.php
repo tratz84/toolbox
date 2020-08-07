66e8d7531998b36b3334447dc5b75ab50b8f96ef
Tim van Weezenbeek <tim@itxplain.nl>
2020-08-07 11:23:58 +0200

    invoice, add meta-dependency

--- modules/invoice/meta.php
+++ modules/invoice/meta.php
@@ -3,5 +3,8 @@
 
 use core\module\ModuleMeta;
 
-return new ModuleMeta('invoice', 'Facturatie/Order module',   'Maken van facturen danwel orders', 40);
+$m = new ModuleMeta('invoice', 'Facturatie/Order module',   'Maken van facturen danwel orders', 40);
+$m->addDependency('customer');
+
+return $m;
 
