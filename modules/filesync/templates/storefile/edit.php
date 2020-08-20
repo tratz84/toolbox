6a6178f6eee54d331dae12e7de78eb6f328846ef
Tim van Weezenbeek <tim@itxplain.nl>
2020-08-20 16:29:22 +0200

    filesync, download link on detail level

--- modules/filesync/templates/storefile/edit.php
+++ modules/filesync/templates/storefile/edit.php
@@ -3,6 +3,7 @@
 	
 	<div class="toolbox">
 		<a href="<?= appUrl('/?m=filesync&c=storefile&id='.$store->getStoreId()) ?>" class="fa fa-chevron-circle-left"></a>
+		<a href="<?= appUrl('/?m=filesync&c=storefile&a=download&inline=1&id='.$form->getWidgetValue('store_file_id')) ?>" target="_blank" class="fa fa-download"></a>
 		<a href="javascript:void(0);" class="fa fa-save submit-form"></a>
 	</div>
 	
