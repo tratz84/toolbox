857831dd64f06280e4796a08c10dc716971d57dc
Tim van Weezenbeek <tim@itxplain.nl>
2020-09-01 20:00:04 +0200

    horde, extra classes

--- modules/webmail/lib/3rdparty/autoload_horde.php
+++ modules/webmail/lib/3rdparty/autoload_horde.php
@@ -2,16 +2,17 @@
 
 
 spl_autoload_register(function($name) {
-    if (strpos($name, 'Horde_') !== 0) {
+    if (strpos($name, 'Horde_') !== 0 && strpos($name, 'Horde\\') !== 0) {
         return;
     }
     
-    $path = str_replace('_', '/', $name).'.php';
+    $path = str_replace(['_', '\\'], '/', $name).'.php';
     
     $path = realpath( __DIR__.'/horde/lib/'.$path );
     
     if ($path && is_file($path) && strpos($path, realpath( __DIR__.'/horde/lib/')) === 0) {
         require_once $path;
     }
+    
 });
 
