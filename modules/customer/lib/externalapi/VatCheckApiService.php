ce09dc987aacfe36d691c24c81b7d72e45a684ae
Tim van Weezenbeek <tim@itxplain.nl>
2020-09-09 15:41:37 +0200

    customer, vat-vies webservice, rewritten with curl for timeout

--- modules/customer/lib/externalapi/VatCheckApiService.php
+++ modules/customer/lib/externalapi/VatCheckApiService.php
@@ -50,23 +50,97 @@ class VatCheckApiService {
         // check soap api
         $countryCode = substr($nr, 0, 2);
         $vatNumber = substr($nr, 2);
+
         
-        try {
-            $soapClient = new \SoapClient('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl');
-            $r = $soapClient->checkVat(array('countryCode' => $countryCode, 'vatNumber' => $vatNumber));
-        } catch (\Exception $ex) {
-            return null;
-        }
+        // implement this with curl
+        $r = $this->callSoapService($countryCode, $vatNumber);
         
         if (is_object($r)) {
             $this->soapCache[$nr] = $r;
             
-            // TODO: wrap response in a class?
-            
-            return $r;
+            // wrap response in a class?
+            if ($r->valid) {
+                return $r;
+            }
+            else {
+                return null;
+            }
         }
         
         return null;
+    }
+    
+    protected function callSoapService($countryCode, $vatNr) {
+        // build soap call
+        $xml = new \DOMDocument();
+        $elEnv = $xml->createElementNS('http://schemas.xmlsoap.org/soap/envelope/', 'soapenv:Envelope');
+        $elEnv->setAttribute('xmlns:urn', 'urn:ec.europa.eu:taxud:vies:services:checkVat:types');
+        
+        $elHeader = $xml->createElement('soapenv:Header');
+        $elEnv->appendChild( $elHeader );
+        
+        
+        $elBody = $xml->createElement('soapenv:Body');
+        $elEnv->appendChild( $elBody );
+        
+        $elCheckVat = $xml->createElement('urn:checkVat');
+        
+        $elCountry = $xml->createElement('urn:countryCode');
+        $elCountry->textContent = $countryCode;
+        $elCheckVat->appendChild( $elCountry );
+        
+        $elVatNr = $xml->createElement('urn:vatNumber');
+        $elVatNr->textContent = $vatNr;
+        $elCheckVat->appendChild( $elVatNr );
+        
+        $elBody->appendChild($elCheckVat);
+        $xml->appendChild($elEnv);
+        
+        
+        $xml = $xml->saveXML(null);
+        $xml = substr($xml, strpos($xml, '?>')+3);
+        
+        
+        // send soap call
+        $ch = curl_init('http://ec.europa.eu/taxation_customs/vies/services/checkVatService');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-type: text/xml; charset=UTF-8']);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);        // timeout, in the past the service failed, while the connection hang infinite
+        curl_setopt($ch, CURLOPT_TIMEOUT,        5);
+        
+        $r = curl_exec($ch);
+        curl_close($ch);
+        
+        
+        // parse
+        $xd = new \DOMDocument();
+        $xd->loadXML($r);
+        $el = $xd->getElementsByTagName('checkVatResponse');
+        
+        $r = new \stdClass();
+        $r->valid = false;
+        if ($el->count()) {
+            
+            $el = $el->item(0)->getElementsByTagName('*');
+            for($x=0; $x < $el->count(); $x++) {
+                $el2 = $el->item($x);
+                
+                if (is_a($el2, \DOMElement::class)) {
+                    $name = $el2->nodeName;
+                    if ($name == 'valid') {
+                        $r->valid = $el2->textContent == 'true' ? true : false;
+                    } else {
+                        $r->{$name} = $el2->textContent;
+                    }
+                }
+            }
+        }
+        
+        return $r;
     }
     
     
