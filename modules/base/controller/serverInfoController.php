1e25fe8a23e93ff64e975b5a7b51b635f286fb54
Tim van Weezenbeek <tim@itxplain.nl>
2020-11-20 12:22:03 +0100

    base, report OS

--- modules/base/controller/serverInfoController.php
+++ modules/base/controller/serverInfoController.php
@@ -12,12 +12,14 @@ class serverInfoController extends BaseController {
         
         $this->sic = object_container_create(ServerInfoContainer::class);
         $this->sic->addInfo('Php version', PHP_VERSION);
+        $this->sic->addInfo('Operating system', php_uname());
         
         if (toolbox_html2pdf_available()) {
             $this->sic->addInfo('wkhtmltopdf', 'Ok');
         } else {
             $this->sic->addInfo('wkhtmltopdf', 'Not available', 'Unable to create PDF-files from HTML');
         }
+        
         
         hook_eventbus_publish( $this->sic, 'base', 'ServerInfoContainer' );
         
