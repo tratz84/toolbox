439d7be7bf2f14f71a4b9d37110fdfa42d2c5b03
Tim van Weezenbeek <tim@itxplain.nl>
2020-11-26 09:32:45 +0100

    base, more serverinfo

--- modules/base/controller/serverInfoController.php
+++ modules/base/controller/serverInfoController.php
@@ -11,18 +11,34 @@ class serverInfoController extends BaseController {
     public function action_index() {
         
         $this->sic = object_container_create(ServerInfoContainer::class);
+        
+        // base info
         $this->sic->addInfo('Php version', PHP_VERSION);
         $this->sic->addInfo('Operating system', php_uname());
+        $this->sic->addInfo('Memory limit', ini_get('memory_limit'));
+        $this->sic->addInfo('Max execution time (Time limit)', ini_get('max_execution_time'));
         
+        // wkhtmltopdf is used for archiving in webmail-mod
         if (toolbox_html2pdf_available()) {
             $this->sic->addInfo('wkhtmltopdf', 'Ok');
         } else {
             $this->sic->addInfo('wkhtmltopdf', 'Not available', 'Unable to create PDF-files from HTML');
         }
         
+        // report PHP_FD_SETSIZE for max. open files + connections
         $this->sic->addInfo('PHP_FD_SETSIZE '.infopopup('max open files + connections'), PHP_FD_SETSIZE );
         
+        // check php-curl
+        $curl = extension_loaded('curl');
+        $this->sic->addInfo('php-curl', $curl?'Ok':'Not loaded');
         
+        // check php-gd
+        $gd = extension_loaded('gd');
+        $this->sic->addInfo('php-gd', $gd?'Ok':'Not loaded');
+        
+        // check php-imagick
+        $imagick = extension_loaded('imagick');
+        $this->sic->addInfo('php-imagic', $imagick?'Ok':'Not loaded');
         
         hook_eventbus_publish( $this->sic, 'base', 'ServerInfoContainer' );
         
