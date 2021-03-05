0a1295dbfb569fca943418bbe42402e31253e92e
Tim van Weezenbeek <tim@itxplain.nl>
2021-03-05 19:03:46 +0100

    bin/runcron, standalone installation support

--- bin/runcron.php
+++ bin/runcron.php
@@ -14,15 +14,22 @@ include dirname(__FILE__).'/../config/config.php';
 
 \core\Context::getInstance()->enableModule('admin');
 
-$ics = ObjectContainer::getInstance()->get(AdminCustomerService::class);
-$customers = $ics->readCustomers();
-
-
-foreach($customers as $c) {
-    $name = $c->getContextName();
-    print "Customer: " . $name . "\n";
-    get_url(BASE_URL . '/' . $name . '/?m=base&c=public/cron&a=run');
+// standalone?
+if (is_standalone_installation()) {
+    get_url(BASE_URL . BASE_HREF . '?m=base&c=public/cron&a=run');
 }
+// multi-installation? => loop all through customers
+else {
+    $ics = ObjectContainer::getInstance()->get(AdminCustomerService::class);
+    $customers = $ics->readCustomers();
+    
+    foreach($customers as $c) {
+        $name = $c->getContextName();
+        print "Customer: " . $name . "\n";
+        get_url(BASE_URL . BASE_HREF . $name . '/?m=base&c=public/cron&a=run');
+    }
+}
+
 
 print "Done\n";
 
