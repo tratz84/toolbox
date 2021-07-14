11d088e96584eb9378d2ac5925d3e46eb3b584e4
Tim van Weezenbeek <tim@itxplain.nl>
2021-07-14 12:23:08 +0200

    customer, menu sorting

--- modules/customer/hook/100-menu.php
+++ modules/customer/hook/100-menu.php
@@ -8,19 +8,22 @@ hook_eventbus_subscribe('base', 'MenuService::listMainMenu', function($src) {
     if (ctx()->isModuleEnabled('customer')) {
         if (ctx()->isCustomersSplit()) {
             $menuCompany = new Menu();
-            $menuCompany->setIconLabelUrl('fa-user', t('Companies'), '/?m=customer&c=company', 200);
+            $menuCompany->setIconLabelUrl('fa-user', t('Companies'), '/?m=customer&c=company');
+            $menuCompany->setWeight(20);
             $menuCompany->setMenuCode('company');
             $src->add($menuCompany);
             
             $menuPerson = new Menu();
-            $menuPerson->setIconLabelUrl('fa-user', t('Persons'), '/?m=customer&c=person', 300);
+            $menuPerson->setIconLabelUrl('fa-user', t('Persons'), '/?m=customer&c=person');
+            $menuPerson->setWeight(21);
             $menuPerson->setMenuCode('person');
             $src->add($menuPerson);
         }
         else {
-            $menuPerson = new Menu();
-            $menuPerson->setIconLabelUrl('fa-user', t('Persons'), '/?m=customer&c=person', 300);
-            $src->add($menuPerson);
+            $menuCustomers = new Menu();
+            $menuCustomers->setIconLabelUrl('fa-user', t('Customers'), '/?m=customer&c=customer');
+            $menuCustomers->setWeight(20);
+            $src->add( $menuCustomers );
         }
     }
 });
