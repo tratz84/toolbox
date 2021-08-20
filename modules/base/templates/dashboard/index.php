9b7eabb8d68e04bf793ab82a0fa45a1bdd4cc187
Tim van Weezenbeek <tim@itxplain.nl>
2021-08-20 12:50:54 +0200

    base, dashboard, removal dev-code

--- modules/base/templates/dashboard/index.php
+++ modules/base/templates/dashboard/index.php
@@ -37,8 +37,6 @@
 var dash = null;
 var dwc = <?= json_encode($dwc) ?>;
 
-//{"widgets":[{"code":"projectRecentHours","name":"Laatste uren projecten","description":"Overzicht van de laatst geregistreerde uren","ajaxUrl":"/project/widget/list.do"},{"code":"calendarUpcoming","name":"Kalender - aankomende afspraken","description":"Toont de opkomende afspraken voor komende 10 werkdagen","ajaxUrl":"/calendar/widget/upcoming.do"},{"code":"todo","name":"ToDo","description":"Overzicht te doen","ajaxUrl":"/calendar/widget/todo.do"},{"code":"webmailLastmail","name":"Laatste 20 e-mails","description":"Overzicht laatste 20 e-mails","ajaxUrl":"/webmail/widget/lastmail.do"},{"code":"lastSupportEntries","name":"Supportaanvragen","description":"Overzicht van laatste 20 supportaanvragen","ajaxUrl":"/support/widget/lastActivities.do"},{"code":"invoicesOutstanding","name":"Openstaande facturen","description":"Overzicht openstaande facturen","ajaxUrl":"/invoice/widget/invoicesoutstanding.do"},{"code":"lastRentalEntries","name":"Laatste wijzigingen verhuur","description":"Overzicht van de laatste wijzigingen in de verhuur","ajaxUrl":"/rental/widget/lastActivityEntries.do"}],"userWidgets":{"todo":{"x":3,"width":3,"y":0,"height":3},"webmailLastmail":{"x":6,"width":6,"y":0,"height":6},"projectRecentHours":{"x":0,"width":6,"y":3,"height":3},"lastSupportEntries":{"x":6,"width":6,"y":6,"height":3},"invoicesOutstanding":{"x":0,"width":6,"y":6,"height":3},"calendarUpcoming":{"x":0,"width":3,"y":0,"height":3}}};
-
 $(document).ready(function() {
 	
 	dash = new Dashboard( '#grid-stack', dwc );
