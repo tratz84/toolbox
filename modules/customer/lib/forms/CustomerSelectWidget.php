f9e4d81ffcd71c045f5d884863388dc1ef5402d8
Tim van Weezenbeek <tim@itxplain.nl>
2021-09-20 15:01:43 +0200

    customer, CustomerSelect-widget, doing smart binding-things ;)

--- modules/customer/lib/forms/CustomerSelectWidget.php
+++ modules/customer/lib/forms/CustomerSelectWidget.php
@@ -69,6 +69,11 @@ class CustomerSelectWidget extends DynamicSelectField {
             }
         }
         
+        // gets kinda messy ;) when widget name is 'person_id' => ignore companyId & visa versa 
+        if ($this->name == 'company_id') $personId = null;
+        if ($this->name = 'person_id') $companyId = null;
+        
+        
         if ($companyId) {
             $this->setValue('company-'.$companyId);
             
