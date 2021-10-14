337353283e8841ebfb250bba7f05a84cef31db62
Tim van Weezenbeek <tim@itxplain.nl>
2021-10-14 15:47:43 +0200

    invoice, update.php cleaned up, old stuff..

--- modules/invoice/update.php
+++ modules/invoice/update.php
@@ -5,13 +5,3 @@ use core\db\DatabaseHandler;
 
 $sql = array();
 
-$sql[] = 'alter table invoice__to_bill change column type type varchar(16)';
-$sql[] = 'update invoice__to_bill set type=\'debet\' where type=\'invoice\'';
-$sql[] = 'update invoice__to_bill set type=\'credit\' where type=\'bill\'';
-
-
-$dbh = DatabaseHandler::getConnection('default');
-
-foreach($sql as $s) {
-   $dbh->query( $s );
-}
