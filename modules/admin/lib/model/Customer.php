49f7599cc509919944d39a0913e012962d811ecd
Tim van Weezenbeek <tim@itxplain.nl>
2021-10-28 10:22:16 +0200

    admin, Customer::getExperimental, apply filter

--- modules/admin/lib/model/Customer.php
+++ modules/admin/lib/model/Customer.php
@@ -6,6 +6,13 @@ namespace admin\model;
 
 class Customer extends base\CustomerBase {
 
+    
+    
+    public function getExperimental() {
+        $r = $this->getField('experimental');
+        
+        return apply_filter(self::class.'::getExperimental', $r);
+    }
 
 }
 
