abbe251fd91f60772b9fbde6887d6d3c598fb377
Tim van Weezenbeek <tim@itxplain.nl>
2022-01-18 09:58:56 +0100

    core, forms error code to text dataimport, error reporting & memory size

--- modules/core/lib/functions/forms.php
+++ modules/core/lib/functions/forms.php
@@ -32,3 +32,40 @@ function render_colorpicker($name, $label, $value) {
 }
 
 
+
+function files_error_to_text( $errNo ) {
+    
+    if (is_array($errNo) && isset($errNo['error'])) {
+        $errNo = $errNo['error'];
+    }
+    
+    if ($errNo == 0) {
+        return null;
+    }
+    
+    
+    if ($errNo === UPLOAD_ERR_INI_SIZE) {
+        return 'The uploaded file exceeds the upload_max_filesize directive';
+    }
+    if ($errNo == UPLOAD_ERR_FORM_SIZE) {
+        return 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
+    }
+    if ($errNo == UPLOAD_ERR_PARTIAL) {
+        return 'The uploaded file was only partially uploaded';
+    }
+    if ($errNo == UPLOAD_ERR_NO_FILE) {
+        return 'No file was uploaded.';
+    }
+    if ($errNo == UPLOAD_ERR_NO_TMP_DIR) {
+        return 'Missing a temporary folder';
+    }
+    if ($errNo == UPLOAD_ERR_CANT_WRITE) {
+        return 'Failed to write file to disk';
+    }
+    if ($errNo == UPLOAD_ERR_EXTENSION) {
+        return 'A PHP extension stopped the file upload';
+    }
+    
+    return 'Unknown error';
+}
+
