939e06dba65e10bfa00fcfda1760b7d52728f39d
Tim van Weezenbeek <tim@itxplain.nl>
2022-01-21 13:14:23 +0100

    dataimport, header-link

--- modules/dataimport/templates/formSelect/index.php
+++ modules/dataimport/templates/formSelect/index.php
@@ -1,6 +1,10 @@
 
 <div class="page-header">
-
+	
+	<div class="toolbox">
+		<a href="<?= appUrl('/?m=base&c=masterdata/index') ?>" class="fa fa-chevron-circle-left"></a>
+	</div>
+	
 	<h1><?= t('Select data type') ?></h1>
 </div>
 
