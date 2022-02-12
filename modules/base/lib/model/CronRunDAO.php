0489d3670a5d1838b4e1bf3fd459fb7fcd7ef6ad
Tim van Weezenbeek <tim@itxplain.nl>
2022-02-12 14:53:08 +0100

    base, cron history, limit fix

--- modules/base/lib/model/CronRunDAO.php
+++ modules/base/lib/model/CronRunDAO.php
@@ -13,8 +13,16 @@ class CronRunDAO extends \core\db\DAOObject {
 	
 	
 	public function readLast($cronId, $no) {
+	    $no = intval($no);
+	    if ($no < 0)
+	        $no = 50;
 	    
-	    return $this->queryList('select * from base__cron_run where cron_id = ? order by created desc', array($cronId));
+	    return $this->queryList('select * 
+                                from base__cron_run 
+                                where cron_id = ? 
+                                order by created desc
+                                limit ' . $no
+	        , array($cronId));
 	    
 	}
 
