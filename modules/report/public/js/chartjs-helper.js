50dfb241a7b1c86ba84fab173ccd23cab469429f
Tim van Weezenbeek <tim@itxplain.nl>
2022-05-24 12:19:11 +0200

    report, create-chart-js

--- modules/report/public/js/chartjs-helper.js
+++ modules/report/public/js/chartjs-helper.js
@@ -1,4 +1,24 @@
 
 
 
+function create_chartjs( id, config ) {
+	var container = $( '#' + id );
+	
+	var parentContainer = $(container).closest('.grid-stack-item-content');
+	
+	$(container).empty();
+	
+	var w = $(container).width();
+	var h = parentContainer.height() - (parentContainer.find('.widget-title').outerHeight());
+	var canvas = $('<canvas />');
+	canvas.attr('width', w);
+	canvas.attr('height', h);
+	container.append( canvas );
+	
+	var ctx = $(canvas).get(0).getContext('2d');
+	
+	var bar = new Chart( ctx, config );
+	
+}
+
 
