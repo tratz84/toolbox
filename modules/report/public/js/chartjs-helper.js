e93d888a90d9e196ff30933b6f7da5755268d0f3
Tim van Weezenbeek <tim@itxplain.nl>
2022-06-01 11:13:39 +0200

    report, chartjs-helper, query adjusted to support non-grid situations

--- modules/report/public/js/chartjs-helper.js
+++ modules/report/public/js/chartjs-helper.js
@@ -33,8 +33,8 @@ function create_chartjs( id, config ) {
 
 
 function report_widget_title( node, title ) {
-	if ($(node).hasClass('grid-stack-item') == false) {
-		node = $(node).closest( '.grid-stack-item' );
+	if ($(node).hasClass('widget-item-content') == false) {
+		node = $(node).closest( '.widget-item-content' );
 	}
 	
 	if ($(node).length == 1) {
