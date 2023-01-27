9cad477d1791d56425b0692e5fb1a97dc01c4519
Tim van Weezenbeek <tim@itxplain.nl>
2023-01-27 19:41:51 +0100

    css stuff

--- modules/base/templates/masterdata/index/index.php
+++ modules/base/templates/masterdata/index/index.php
@@ -4,22 +4,19 @@
 </div>
 
 
-<div class="functions-menu-page">
+<div id="menu-container">
 
-	<div id="menu-container">
-		<div ez-for="menu" ez-key="title" ez-item="m">
-			<div class="col-12 col-sm-4 col-lg-3 setting-menu-tag-container">
-				<h2>{{ title }}</h2>
-				
-				<ul class="" ez-for="m" ez-item="mi">
-					<li><a href="javascript:window.location = appUrl( '{{ mi.url }}' );">{{ mi.title }}</a></li>
-				</ul>
-			</div>
+	<div class="functions-menu-page" ez-for="menu" ez-key="title" ez-item="m">
+		<div class="col-12 col-sm-4 col-lg-3 setting-menu-tag-container">
+			<h2>{{ title }}</h2>
+		
+			<ul class="" ez-for="m" ez-item="mi">
+				<li><a href="javascript:window.location = appUrl( '{{ mi.url }}' );">{{ mi.title }}</a></li>
+			</ul>
 		</div>
 	</div>
-
-	<div class="clearfix"></div>
 </div>
+
 
 <script>
 
