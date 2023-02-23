e914beedc3b716116335a54f8e22fb3261658843
Tim van Weezenbeek <tim@itxplain.nl>
2023-02-23 17:45:28 +0100

    table-select-widget, code cleanup

--- modules/base/public/js/table-select-widget.js
+++ modules/base/public/js/table-select-widget.js
@@ -23,7 +23,7 @@ class TableSelectWidget {
 			<span class="cst-selector-caret fa fa-caret-down"></span>
 		</span>
 		<div class="cst-dropdown-container">
-			<div><input type="text" name="q" [placeholder]="txt_search" data-prevent-submit="1" autocomplete="off" /></div>
+			<div><input type="text" name="q" placeholder="{{ toolbox_t('Search...') }}" data-prevent-submit="1" autocomplete="off" /></div>
 			<div ez-subtemplate="result-container">
 				<table ez-if="results.length > 0" class="list-response-table">
 					<thead>
@@ -53,8 +53,6 @@ class TableSelectWidget {
 		this.container = container;
 		
 		this.container.tsw = this;
-		
-		this.vars['txt_search'] = toolbox_t('Search...');
 		
 		this.vars['name']        = $(this.container).attr('name');
 		this.vars['value']       = $(this.container).attr('value');
