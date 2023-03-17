e1ef407c38c2967666264f18334997aa1fde231a
Tim van Weezenbeek <tim@itxplain.nl>
2023-03-17 10:24:54 +0100

    base, group, dev...

--- modules/base/templates/group/index.php
+++ modules/base/templates/group/index.php
@@ -12,7 +12,12 @@
 </div>
 
 
-<div id="tree"></div>
+<div>
+	<div id="tree"></div>
+	
+	<div id="group-summary"></div>
+	
+</div>
 
 
 <script>
@@ -22,7 +27,21 @@ let json_tree = <?= json_encode( $groupTree, JSON_PRETTY_PRINT ) ?>;
 $('#tree').jstree( {'core': {
 	'data': json_tree
 }} );
-
+$('#tree').on('select_node.jstree', function(n, data) {
+	if ( data.node ) {
+		
+	}
+});
+$('#tree').on('dblclick.jstree', function(n, data) {
+	console.log('wel hier');
+	let l = $('#tree').jstree(true).get_selected(true);
+	
+	console.log( l );
+	
+	if (l && l.length > 0) {
+		window.location = appUrl('/?m=base&c=group&a=edit&id='+l[0].id);
+	}
+});
 
 </script>
 
