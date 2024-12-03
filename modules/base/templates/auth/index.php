1693d2272eed898dfcd39f02a967489117fdd5af
Tim van Weezenbeek <tim@itxplain.nl>
2024-12-03 14:02:52 +0100

    auth page, redesign for translations in pro... project

--- modules/base/templates/auth/index.php
+++ modules/base/templates/auth/index.php
@@ -19,36 +19,35 @@
 		</div>
 		<?php endif; ?>
 	
-    	<table>
-    		<tr>
-    			<th><?= t('Username') ?></th>
-    			<td><input type="text" name="username" value="<?= esc_attr($username) ?>" autofocus /></td>
-    		</tr>
+    	<div>
+    		<div class="input-row">
+    			<div class="label"><?= t('Username') ?></div>
+    			<div><input type="text" name="username" value="<?= esc_attr($username) ?>" autofocus /></div>
+    		</div>
     		
-    		<tr>
-    			<th><?= t('Password') ?></th>
-    			<td><input type="password" name="p" value="<?= esc_attr($password) ?>" /></td>
-    		</tr>
-    		<tr>
-    			<th></th>
-    			<td>
-    				<label>
-    					<?= t('Remember me') ?>
-    					<?= render_checkbox('rememberme', ['checked' => $remembermeChecked]) ?>
-					</label>
-    			</td>
-    		</tr>
+    		<div class="input-row">
+    			<div class="label"><?= t('Password') ?></div>
+    			<div><input type="password" name="p" value="<?= esc_attr($password) ?>" /></div>
+    		</div>
     		<?php if (isset($error)) : ?>
-    		<tr>
-    			<td colspan="2" class="error"><?= $error ?></td>
-    		</tr>
+    		<div>
+    			<div class="error"><?= $error ?></div>
+    		</div>
     		<?php endif; ?>
-    		<tr>
-    			<td colspan="2" align=right>
+    		<div>
+    			<div>
     				<input type="submit" value="<?= t('Logon') ?>" />
-    			</td>
-    		</tr>
-    	</table>
+    			</div>
+    		</div>
+    		
+    		<div class="remember-me">
+				<label>
+					<?= render_checkbox('rememberme', ['checked' => $remembermeChecked]) ?>
+					
+					<?= t('Remember me') ?>
+				</label>
+    		</div>
+    	</div>
     	
     	<br/>
     	<br/>
