65feaabdd042b254b623b1e44ad9e07dd57e1931
Tim van Weezenbeek <tim@itxplain.nl>
2025-04-23 15:30:10 +0200

    core, AuthorizationException, nullable fix

--- modules/core/lib/exception/AuthorizationException.php
+++ modules/core/lib/exception/AuthorizationException.php
@@ -5,7 +5,7 @@ namespace core\exception;
 class AuthorizationException extends \Exception {
     
     
-    public function __construct($message = null, $code = 0, \Exception $previous = null) {
+    public function __construct($message = null, $code = 0, ?\Exception $previous = null) {
         parent::__construct($message, $code, $previous);
     }
     
