464f262a285d50918ffc0d8ef1125c16604ebfe5
Tim van Weezenbeek <tim@itxplain.nl>
2025-05-14 15:16:42 +0200

    core, deprecated null-stuff

--- modules/core/lib/exception/InvalidArgumentException.php
+++ modules/core/lib/exception/InvalidArgumentException.php
@@ -4,7 +4,7 @@ namespace core\exception;
 
 class InvalidArgumentException extends \Exception {
     
-    public function __construct($message = null, $code = 0, \Exception $previous = null) {
+    public function __construct(?string $message = null, $code = 0, ?\Exception $previous = null) {
         parent::__construct($message, $code, $previous);
     }
     
