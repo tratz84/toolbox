2c740079772d9fa17bc53edec92ca78d0b02143e
Tim van Weezenbeek <tim@itxplain.nl>
2025-11-20 14:24:19 +0100

    doc, removal unused code

--- doc/sql/basedata.sql
+++
@@ -1,8 +0,0 @@
-
-
--- create user admin, with password test123
-INSERT INTO `base__user` VALUES (NULL, 'admin', 'demo@demo.itxplain.nl', 'test123', now(), now(), 'admin', '', 'demo', NULL, NULL);
-
-
-
-
